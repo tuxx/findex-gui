62d04d8af1a1b1665ac58e3636d2c101771b37ba
Sander Ferdinand <sa.ferdinand@gmail.com>
2016-12-27 07:48:59 +0100

    status

--- findex_gui/controllers/browse/routes.py
+++ findex_gui/controllers/browse/routes.py
@@ -4,7 +4,7 @@ from findex_gui import app, themes
 import findex_gui.controllers.browse.converters
 from findex_gui.controllers.browse.browse import Browse
 from findex_gui.controllers.resources.resources import ResourceController
-from findex_common.static_variables import FileProtocols
+from findex_common.static_variables import FileProtocols, ResourceStatus
 from findex_common.utils_time import TimeMagic
 
 @app.route('/browse')
@@ -16,6 +16,7 @@ def browse_home():
     for r in resources:
         setattr(r, "protocol_human", FileProtocols().name_by_id(r.protocol))
         setattr(r, "ago", TimeMagic().ago_dt(r.date_crawl_end))
+        setattr(r, "status_human", ResourceStatus().name_by_id(r.meta.status))
 
     return themes.render('main/browse', resources=resources)
 
@@ -32,6 +33,6 @@ def browse(parsed):
         return themes.render('main/browse_dir', **data)
     else:
         return """
-        I have not made this page yet. The file browser has a 'src' link in the table.
+        The file browser has a 'src' link in the table.
         <a href="javascript:history.back()">Please click here</a> to go back.
         """

7c49b85388843af2095cbf6600ccb456d57dcce7
Sander Ferdinand <sa.ferdinand@gmail.com>
2017-05-11 10:23:07 +0200

    Simple decorator for API routes

--- findex_gui/controllers/helpers.py
+++ findex_gui/controllers/helpers.py
@@ -1,6 +1,89 @@
-from flask import request, url_for
+from flask import request, url_for, jsonify
+from datetime import datetime
 
 from findex_gui import app
+from findex_common.utils import decorator_parametrized
+# from findex_gui.controllers.user.decorators import _not_logged_in
+# from findex_gui.controllers.auth.auth import get_current_user_data, not_logged_in
+
+
+@decorator_parametrized
+def findex_api(f, *api_arguments):
+    """
+    Simple WIP decorator for API routes.
+
+    Decorated API functions should:
+     On success: return any of: str, int, float, list, dict.
+     On failure: return an exception
+    """
+    def rep():
+        incoming = {}
+        outgoing = {}
+        if request.method == "POST":
+            if request.form:
+                incoming = request.form.to_dict()
+            elif request.json:
+                incoming = request.json
+            else:
+                raise Exception("Wrong parameters")
+        elif request.method == "GET":
+            # parse GET vars here
+            e = ""
+        else:
+            raise Exception("unsupported method")
+
+        for api_argument in api_arguments:
+            if api_argument.key in incoming:
+                value = incoming.get(api_argument.key)
+
+                if type(value) != api_argument.type:
+                    if issubclass(api_argument.type, int):
+                        if value.isdigit():
+                            value = int(value)
+                    elif issubclass(api_argument.type, float):
+                        try:
+                            value = float(value)
+                        except ValueError:
+                            pass
+
+                    if type(value) != api_argument.type:
+                        raise Exception("wrong type for argument \"%s\", "
+                                        "should be of type \"%s\"" % (api_argument.key,
+                                                                      str(api_argument.type)))
+                outgoing[api_argument.key] = value
+            else:
+                if api_argument.required:
+                    raise Exception("argument \"%s\" is required" % api_argument.key)
+
+        api_result = f(outgoing)
+        if isinstance(api_result, (int, float, str, list, dict, datetime)):
+            return jsonify({"status": True, "data": api_result})
+        else:
+            return jsonify({"status": False, "data": api_result})
+    return rep
+
+
+class ApiArgument:
+    def __init__(self, *args, **kwargs):
+        self.key = args[0]
+
+        if "type" not in kwargs or not kwargs["type"]:
+            raise Exception("type required")
+
+        if not issubclass(kwargs["type"], (int, dict, float, str, datetime)):
+            raise Exception("bad type for argument %s" % self.key)
+
+        if "help" not in kwargs or not kwargs["help"]:
+            raise Exception("help required")
+
+        if "required" not in kwargs:
+            self.required = False
+        else:
+            self.required = kwargs["required"]
+
+        self.type = kwargs["type"]
+        self.help = kwargs["help"]
+        self.kwargs = kwargs
 
 
 def redirect_url(default='index'):

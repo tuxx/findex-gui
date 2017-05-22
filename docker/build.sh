a82b602d82f44d93f9ed0297b554e9c8b8d9e3ec
Arnold Vriezekolk <arnold.vriezekolk@drukwerkdeal.nl>
2017-05-22 13:26:43 +0200

    Updated build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,5 @@
 docker build zomboPG/ -t zombopg:0.1 --label zomboPG:0.1
 docker build zomboES -t zomboes:0.1 --label zomboES:0.1
+mkdir -p findex/findex/
 cp -r ../findex_gui/* findex/findex/
 docker build findex -t findexgui --label findexgui:0.1

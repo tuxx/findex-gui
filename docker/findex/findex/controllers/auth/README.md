a6f25f6dc0c1b89a0afd3bd6a5ea859b06242f7f
Arnold Vriezekolk <arnold.vriezekolk@drukwerkdeal.nl>
2017-05-22 13:26:10 +0200

    Removed all the docker/findex/findex/ files

--- docker/findex/findex/controllers/auth/README.md
+++
@@ -1,6 +0,0 @@
-Hardforked from https://pypi.python.org/pypi/Flask-Auth/0.85
-
-- Changed algo to SHA1
-- Python 3 support
-
-Original code by `Lars de Ridder` - 2012
--- docker/findex/findex/themes/_admin/static/js/admin.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-    // Collapse sidebar menu on the currently active item
-    var document_location_pathname = document.location.pathname.replace('#', '');
-    var menu_selection = $('.sidebar-menu [href="'+document_location_pathname+'"]');
-    menu_selection.parent().parent().parent().addClass('active');
-    menu_selection.parent().addClass('active');
-});

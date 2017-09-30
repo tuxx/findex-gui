a8b629a1346dc1fa824fe55531039a323b825c3c
Sander Ferdinand <sa.ferdinand@gmail.com>
2017-09-30 15:46:55 +0200

    Some news stuff

--- findex_gui/controllers/news/routes.py
+++ findex_gui/controllers/news/routes.py
@@ -1,7 +1,7 @@
 from findex_gui.web import app, themes
 from findex_gui.controllers.news.news import NewsController
 
-@app.route("/")
-def root():
-    posts = NewsController.get(limit=5, offset=0)
+@app.route("/news")
+def news():
+    posts = NewsController.get(limit=10, offset=0)
     return themes.render("main/news", posts=posts)

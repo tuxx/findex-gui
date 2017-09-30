0fbf23d7596e756e4caeec5d8fcde4a27ca2d0fc
Sander Ferdinand <sa.ferdinand@gmail.com>
2017-09-30 16:09:54 +0200

    Updated AdminLTE bootstrap theme to 2.4

--- findex_gui/themes/_admin/static/js/admin.js
+++ findex_gui/themes/_admin/static/js/admin.js
@@ -5,3 +5,15 @@ $(function(){
     menu_selection.parent().parent().parent().addClass('active');
     menu_selection.parent().addClass('active');
 });
+
+function msgbox_error(msg){
+    $("div.box-body").prepend(`
+    <div class="col-xs-4 no-padding">
+        <div class="alert alert-danger alert-dismissible">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+            <h4><i class="icon fa fa-ban"></i> Error!</h4>
+            ${msg}
+        </div>
+    </div>
+    `);
+}

173d051ad77a42541ded5f22e2d65beec0dbdf9d
Sander Ferdinand <sa.ferdinand@gmail.com>
2017-10-12 19:33:09 +0200

    Stopped writing sane commit messages

---
+++ findex_gui/controllers/resources/groups.py
@@ -0,0 +1,67 @@
+from sqlalchemy_utils import escape_like
+
+from findex_gui.web import db
+from findex_gui.orm.models import Resource, ResourceMeta, ResourceGroup, Server, Mq
+from findex_gui.controllers.user.roles import role_req
+
+
+class ResourceGroupController:
+    @staticmethod
+    def get(uid: int = None, name: str = None, limit: int = None,
+            offset: int = None, search: str = None):
+        """
+        Fetches some resourcegroups
+        :param uid:
+        :param name:
+        :param limit:
+        :param offset:
+        :param search: performs a fulltext search
+        :return:
+        """
+        q = db.session.query(ResourceGroup)
+
+        if isinstance(uid, int):
+            return [q.filter(ResourceGroup.id == uid).first()]
+
+        if isinstance(name, str) and name:
+            q = q.filter(ResourceGroup.name == name).first()
+
+        if isinstance(search, str) and search:
+            q = q.filter(ResourceGroup.name.ilike("%" + escape_like(search) + "%"))
+
+        if offset and isinstance(offset, int):
+            q = q.offset(offset)
+
+        if limit and isinstance(limit, int):
+            q = q.limit(limit)
+
+        return q.all()
+
+    @staticmethod
+    @role_req("USER_REGISTERED", "RESOURCE_REMOVE", "RESOURCE_CREATE")
+    def assign_amqp(resourcegroup_id: int, amqp_id: int):
+        """
+        Assigns an AMQP broker to a resource group
+        :param amqp_id:
+        :param resourcegroup_id: e
+        :return:
+        """
+        resourcegroup_id, amqp_id = map(int, [resourcegroup_id, amqp_id])
+
+        rq = db.session.query(ResourceGroup)
+        rq = rq.filter(ResourceGroup.id == resourcegroup_id)
+        rq = rq.first()
+
+        if not rq:
+            raise Exception("ResourceGroup id %d not found" % resourcegroup_id)
+
+        aq = db.session.query(Mq)
+        aq = aq.filter(Mq.id == amqp_id)
+        aq = aq.first()
+
+        if not aq:
+            raise Exception("Mq id %d not found" % amqp_id)
+
+        rq.mq = aq
+        db.session.commit()
+        db.session.flush()

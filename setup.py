406309bb0f739a0974aad1fbe87df8ff9b73cecd
Sander Ferdinand <sa.ferdinand@gmail.com>
2017-11-05 22:51:26 +0100

    bumped version

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ def do_setup(**kwargs):
 
 do_setup(
     name="Findex-GUI",
-    version="0.3.0.1",
+    version="0.3.0.2",
     author="Sander Ferdinand",
     author_email="sa.ferdinand@gmail.com",
     packages=[
